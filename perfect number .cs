a8a7dc7b8fa6ebc0c8c03207113c8e7dc430fcde
Mohamedsalah2004 <121448604+Mohamedsalah2004@users.noreply.github.com>
2022-12-26 18:03:49 +0200

    Add files via upload

---
+++ perfect number .cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Salah
+{
+    public class Mohamed 
+    {         
+         public static void Main()
+        {
+        	
+            int i,y,sum=0;
+            Console.WriteLine("Enter the first number");
+			int start =int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the second number");
+			int end =int.Parse(Console.ReadLine());
+			
+			sum=0;
+			
+				for( i=start; i<=end; i++)
+			{
+				for( y=1; y<=end; y++)
+				{
+					if(y<i)
+					{
+						if(i%y==0)
+						sum = sum + y;
+					}
+				}
+				if(sum==i)
+				{
+					Console.WriteLine(i + "   is perfect number");
+				}
+			    
+				sum =0;
+			}
+			return;
+			
+
+        }
+
+    }
+}
---
+++ prime numbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HelloWold
+{
+    public static class Program 
+    {         
+        public static void Main()
+        {
+           	Console.WriteLine("Enter the first number");
+			int start =int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the second number");
+			int end =int.Parse(Console.ReadLine());
+			
+			
+	     	int a =0;
+			for(int i=start; i<=end; i++)
+			{
+				for(int q=1; q<=i; q++)
+				{
+					if(i%q==0)
+					a++;
+				}
+				if(a==2)
+				{
+					Console.WriteLine(i + "  is prime number");
+					a=0;
+				}
+				else
+				{
+					a=0;
+					continue;
+				}
+			}
+        }
+    }
+}
